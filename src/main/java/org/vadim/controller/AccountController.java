de4ff3a004afaad6b5a47eb8df2cd6db39c60903
Krapivin Vadim <krapivinvadim4@gamil.com>
2026-08-20 16:09:12 +0300

    Add doc

--- src/main/java/org/vadim/controller/AccountController.java
+++ src/main/java/org/vadim/controller/AccountController.java
@@ -1,5 +1,9 @@
 package org.vadim.controller;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +28,17 @@ public class AccountController {
   }
 
   @PostMapping("/reg")
+  @ApiResponses( value = {
+      @ApiResponse(
+          responseCode = "201",
+          description = "Успешная регистрация пользователя",
+          content = @Content(schema = @Schema(implementation = AuthResponseDto.class))
+      ),
+      @ApiResponse(
+          responseCode = "401",
+          description = "Пользователь с таким username/email уже есть"
+      )
+  })
   public ResponseEntity<AuthResponseDto> register(
       @RequestBody @Validated(AccountCredentialsDto.Register.class) AccountCredentialsDto accountCredentialsDto){
     return ResponseEntity.status(HttpStatus.CREATED).body(
@@ -32,6 +47,17 @@ public class AccountController {
   }
 
   @PostMapping("/auth")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "Пользователь успешно авторизован",
+          content = @Content(schema = @Schema(implementation = AuthResponseDto.class))
+      ),
+      @ApiResponse(
+          responseCode = "403",
+          description = "Неверные учетные данные пользователя"
+      )
+  })
   public ResponseEntity<AuthResponseDto> auth(
       @RequestBody @Validated(AccountCredentialsDto.Auth.class) AccountCredentialsDto accountCredentialsDto){
     return ResponseEntity.status(HttpStatus.OK).body(
--- src/main/java/org/vadim/dto/AccountCredentialsDto.java
+++ src/main/java/org/vadim/dto/AccountCredentialsDto.java
@@ -1,5 +1,6 @@
 package org.vadim.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.groups.Default;
@@ -11,16 +12,32 @@ import org.vadim.validation.annotation.TelegramTagValidate;
 public record AccountCredentialsDto(
 
     @NotBlank(groups = Register.class)
+    @Schema(
+        description = "Имя пользователя",
+        example = "User"
+    )
     String username,
 
     @NotBlank(groups = Register.class)
     @Email
+    @Schema(
+        description = "Адрес электронной почты пользователя",
+        example = "user@example.com"
+    )
     String email,
 
     @TelegramTagValidate(groups = Register.class)
+    @Schema(
+        description = "Тэг пользователя в Telegram",
+        example = "@User"
+    )
     String tg,
 
     @NotBlank
+    @Schema(
+        description = "Пароль пользователя",
+        example = "user123"
+    )
     String password
 ) {
   public interface Auth extends Default{};
